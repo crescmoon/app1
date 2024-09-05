85ac7fca338cad492b1ca544c37450250fb3871f
crescmoon <crescentmoon8569@gmail.com>
2024-09-06 01:07:07 +0900

    make static template

--- src/App.js
+++ src/App.js
@@ -1,23 +1,111 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {TETROMINOS, Tetromino, getCellClassName} from './TetrisInternal';
+
+const GRID_WIDTH = 10;
+const GRID_HEIGHT = 20;
 
 function App() {
+  const ImgLoader = (props) => {
+    return (
+      <img
+        src={require(`./tetrominos/${props.name}.png`)}
+        alt={props.name}
+        style={{
+          width: "80px",
+          height: "80px",
+          paddingLeft: "20px",
+          paddingTop: props.paddingTop,
+          paddingBottom: props.paddingBottom
+        }}
+      />
+    );
+  };
+
+  const GridLoader = (props) => {
+    return (
+      <div className="tetris-grid" style={{padding: "20px"}}>
+        {grid.map((row, rowIndex) => (
+          <div key={rowIndex} className="tetris-row">
+            {row.map((cell, colIndex) => (
+              <div
+                key={colIndex}
+                className={getCellClassName(cell)}
+              ></div>
+            ))}
+          </div>
+        ))}
+      </div>
+    )
+  };
+
+  const createGrid = () => {
+    const grid = [];
+    for (let r = 0; r < GRID_HEIGHT; r++) {
+      grid.push([]);
+      for (let c = 0; c < GRID_WIDTH; c++) {
+        grid[r].push(0);
+      }
+    }
+    return grid;
+  }
+
+  const grid = createGrid();
+
+  let tetrominos = [
+    new Tetromino(
+      { x: 6, y: 12 },
+      TETROMINOS.T,
+      0
+    )
+  ];
+
+  tetrominos.forEach((tetromino) => {
+    tetromino.getCellCoordinates().forEach(cell => {
+      if (cell.x >= 0 && cell.x < GRID_WIDTH && cell.y >= 0 && cell.y < GRID_HEIGHT) {
+        grid[cell.y][cell.x] = tetromino.type;
+      }
+    });
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="game-wrapper">
+        <div className="next-panel">
+          <h3 style={{color: "#fff", textAlign: "center"}}>
+            Next
+          </h3>
+          <div className="box">
+            <ImgLoader name="I" paddingTop="20px" paddingBottom="20px" />
+          </div>
+        </div>
+        <div className="center-panel">
+          <h2 style={{color: "#fff", textAlign: "center"}}>
+            TETRIS
+          </h2>
+          <GridLoader />
+        </div>
+        <div className="right-panel">
+          <h3 style={{color: "#fff", textAlign: "center"}}>
+            Upcoming
+          </h3>
+          <div className="box">
+            <ImgLoader name="T" paddingTop="20px" />
+            <ImgLoader name="Z" />
+            <ImgLoader name="I" />
+            <ImgLoader name="J" />
+            <ImgLoader name="O" paddingBottom="20px" />
+          </div>
+          <h3 style={{color: "#fff", textAlign: "center"}}>
+            Score
+          </h3>
+          <div className="box">
+            <h3 style={{color: "#fff", textAlign: "center"}}>
+              0
+            </h3>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/TetrisInternal.js
@@ -0,0 +1,143 @@
+export const TETROMINOS = Object.freeze({
+  I: 1,
+  J: 2,
+  L: 3,
+  O: 4,
+  S: 5,
+  T: 6,
+  Z: 7
+})
+
+export class Tetromino {
+  constructor(origin, type, direction) {
+    this.origin = origin;
+    this.type = type;
+    this.direction = direction;
+  }
+  
+  getCellCoordinates() {
+    let type = this.type;
+    let origin = this.origin;
+    let direction = this.direction;
+    let alt = [];
+    switch (type) {
+      case TETROMINOS.I:
+        switch (direction) {
+          case 0: alt = [{x: -1, y: 0}, {x: 0, y: 0}, {x: 1, y: 0}, {x: 2, y: 0}]; break;
+          case 1: alt = [{x: 0, y: -1}, {x: 0, y: 0}, {x: 0, y: 1}, {x: 0, y: 2}]; break;
+          case 2: alt = [{x: -2, y: 0}, {x: -1, y: 0}, {x: 0, y: 0}, {x: 1, y: 0}]; break;
+          case 3: alt = [{x: 0, y: -2}, {x: 0, y: -1}, {x: 0, y: 0}, {x: 0, y: 1}]; break;
+          default: alt = [];
+        }
+        break;
+      case TETROMINOS.J:
+        switch (direction) {
+          case 0: alt = [{x: -1, y: -1}, {x: -1, y: 0}, {x: 0, y: 0}, {x: 1, y: 0}]; break;
+          case 1: alt = [{x: 0, y: -1}, {x: 1, y: -1}, {x: 0, y: 0}, {x: 0, y: 1}]; break;
+          case 2: alt = [{x: -1, y: 0}, {x: 0, y: 0}, {x: 1, y: 0}, {x: 1, y: 1}]; break;
+          case 3: alt = [{x: 0, y: -1}, {x: 0, y: 0}, {x: 0, y: 1}, {x: -1, y: 1}]; break;
+          default: alt = [];
+        }
+        break;
+      case TETROMINOS.L:
+        switch (direction) {
+          case 0: alt = [{x: 1, y: -1}, {x: -1, y: 0}, {x: 0, y: 0}, {x: 1, y: 0}]; break;
+          case 1: alt = [{x: 0, y: -1}, {x: 0, y: 0}, {x: 0, y: 1}, {x: 1, y: 1}]; break;
+          case 2: alt = [{x: -1, y: 0}, {x: 0, y: 0}, {x: 1, y: 0}, {x: -1, y: 1}]; break;
+          case 3: alt = [{x: -1, y: -1}, {x: 0, y: -1}, {x: 0, y: 0}, {x: 0, y: 1}]; break;
+          default: alt = [];
+        }
+        break;
+      case TETROMINOS.O:
+        switch (direction) {
+          case 0: alt = [{x: 0, y: 0}, {x: 1, y: 0}, {x: 0, y: 1}, {x: 1, y: 1}]; break;
+          case 1: alt = [{x: -1, y: 0}, {x: 0, y: 0}, {x: -1, y: 1}, {x: 0, y: 1}]; break;
+          case 2: alt = [{x: -1, y: -1}, {x: 0, y: -1}, {x: -1, y: 0}, {x: 0, y: 0}]; break;
+          case 3: alt = [{x: 0, y: -1}, {x: 1, y: -1}, {x: 0, y: 0}, {x: 1, y: 0}]; break;
+          default: alt = [];
+        }
+        break;
+      case TETROMINOS.S:
+        switch (direction) {
+          case 0: alt = [{x: 0, y: -1}, {x: 1, y: -1}, {x: -1, y: 0}, {x: 0, y: 0}]; break;
+          case 1: alt = [{x: 0, y: -1}, {x: 0, y: 0}, {x: 1, y: 0}, {x: 1, y: 1}]; break;
+          case 2: alt = [{x: 0, y: 0}, {x: 1, y: 0}, {x: -1, y: 1}, {x: 0, y: 1}]; break;
+          case 3: alt = [{x: -1, y: -1}, {x: -1, y: 0}, {x: 0, y: 0}, {x: 0, y: 1}]; break;
+          default: alt = [];
+        }
+        break;
+      case TETROMINOS.T:
+        switch (direction) {
+          case 0: alt = [{x: 0, y: -1}, {x: -1, y: 0}, {x: 0, y: 0}, {x: 1, y: 0}]; break;
+          case 1: alt = [{x: 0, y: -1}, {x: 0, y: 0}, {x: 1, y: 0}, {x: 0, y: 1}]; break;
+          case 2: alt = [{x: -1, y: 0}, {x: 0, y: 0}, {x: 1, y: 0}, {x: 0, y: 1}]; break;
+          case 3: alt = [{x: 0, y: -1}, {x: -1, y: 0}, {x: 0, y: 0}, {x: 0, y: 1}]; break;
+          default: alt = [];
+        }
+        break;
+      case TETROMINOS.Z:
+        switch (direction) {
+          case 0: alt = [{x: -1, y: -1}, {x: 0, y: -1}, {x: 0, y: 0}, {x: 1, y: 0}]; break;
+          case 1: alt = [{x: 1, y: -1}, {x: 0, y: 0}, {x: 1, y: 0}, {x: 0, y: 1}]; break;
+          case 2: alt = [{x: -1, y: 0}, {x: 0, y: 0}, {x: 0, y: 1}, {x: 1, y: 1}]; break;
+          case 3: alt = [{x: 0, y: -1}, {x: -1, y: 0}, {x: 0, y: 0}, {x: -1, y: 1}]; break;
+          default: alt = [];
+        }
+        break;
+        default:
+          alt = [];
+    }
+    return alt.map(cell => ({
+      x: origin.x + cell.x,
+      y: origin.y + cell.y
+    }));
+  }
+
+  defaultRotation(cw) {
+    if (cw) {
+      this.direction = (this.direction + 1) % 4;
+    } else {
+      this.direction = (this.direction + 3) % 4;
+    }
+    if (this.type === TETROMINOS.I) {
+      if (cw) {
+        switch (this.direction) {
+          case 0: this.origin.y -= 1; break;
+          case 1: this.origin.x += 1; break;
+          case 2: this.origin.y += 1; break;
+          case 3: this.origin.x -= 1; break;
+          default: break;
+        }
+      } else {
+        switch (this.direction) {
+          case 0: this.origin.x -= 1; break;
+          case 1: this.origin.y -= 1; break;
+          case 2: this.origin.x += 1; break;
+          case 3: this.origin.y += 1; break;
+          default: break;
+        }
+      }
+    }
+  }
+}
+
+export function getCellClassName(type) {
+  switch (type) {
+    case TETROMINOS.I:
+      return 'tetris-cell I';
+    case TETROMINOS.J:
+      return 'tetris-cell J';
+    case TETROMINOS.L:
+      return 'tetris-cell L';
+    case TETROMINOS.O:
+      return 'tetris-cell O';
+    case TETROMINOS.S:
+      return 'tetris-cell S';
+    case TETROMINOS.T:
+      return 'tetris-cell T';
+    case TETROMINOS.Z:
+      return 'tetris-cell Z';
+    default:
+      return 'tetris-cell';
+  }
+}
